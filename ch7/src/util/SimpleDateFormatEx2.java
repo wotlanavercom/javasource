0ee11e243bb5d59701124e857b5ef265c3bf895e
shin <wotla3009@naver.com>
2023-03-16 17:36:45 +0900

    SimpleDataFormat 주석 추가

--- ch7/src/util/SimpleDateFormatEx2.java
+++ ch7/src/util/SimpleDateFormatEx2.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class SimpleDateFormatEx2 {
 
 	public static void main(String[] args) {
-		
+		//시스템 날짜와 시간 가져오기
 		Date date = new Date();
 		System.out.println(date);
 		
