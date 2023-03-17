67c46af917d5954d290c7de7b25cc3fae5a8f976
shin <wotla3009@naver.com>
2023-03-17 10:54:13 +0900

    생성자 작성

--- ch7/src/util/Test.java
+++ ch7/src/util/Test.java
@@ -6,6 +6,14 @@ public class Test {
 	private int no;
 	private String tel;
 	
+	
+	
+	public Test(String name, int no, String tel) {
+		super();
+		this.name = name;
+		this.no = no;
+		this.tel = tel;
+	}
 	public void print() {}
 	public int sum() {
 		return 0;
