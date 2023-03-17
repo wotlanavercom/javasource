96eff71f495db967f9e4fb173e9aa1a81e739509
shin <wotla3009@naver.com>
2023-03-17 11:01:36 +0900

    생성자와 toString 오버라이드

--- ch7/src/util/Test.java
+++ ch7/src/util/Test.java
@@ -6,9 +6,21 @@ public class Test {
 	private int no;
 	private String tel;
 	
+	
+	
+	public Test(String name, int no, String tel) {
+		super();
+		this.name = name;
+		this.no = no;
+		this.tel = tel;
+	}
 	public void print() {}
 	public int sum() {
 		return 0;
+	}
+	@Override
+	public String toString() {
+		return "Test [name=" + name + ", no=" + no + ", tel=" + tel + "]";
 	}
 	
   
